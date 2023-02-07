b52377b95326f526dec4deba3df77336c8070cc3
joao garcia <jiboiadamor@gmail.com>
2023-02-07 12:36:08 +0000

    created users controller and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: 'Signup successfully.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to '/', notice: 'User was successfully destroyed.'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # USER ROUTES
+  get 'signup', to: 'users#new'
+  post 'users/create', to: 'users#create'
+  get 'users/:id/edit', to: 'users#edit'
+  patch 'users/update', to: 'users#update'
+  get 'users/:id', to: 'users#show'
+  delete 'users/destroy'
 end
